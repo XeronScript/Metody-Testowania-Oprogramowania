c9e36b4a3793c594a84ca5a5c68967fb5a68e24d
XeronScript <82675884+XeronScript@users.noreply.github.com>
2023-02-28 18:40:45 +0100

    Add swapcase

--- lab1/main.py
+++ lab1/main.py
@@ -2,21 +2,22 @@
 
 import sys
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
+def my_printf(format_string, param):
+    shouldDo = True
+
+    for i in range(len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+            if format_string[i] == '#' and format_string[i+1] == 'k':
+                param = param.swapcase()
                 print(param,end="")
                 shouldDo=False
             else:
-                print(format_string[idx],end="")
+                print(format_string[i], end="")
         else:
             shouldDo=True
     print("")
 
-data=sys.stdin.readlines()
+data = sys.stdin.readlines()
 
 for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
