256d7895324f90e7d49db777f6dbed135fd2e979
Wartutel <82675884+XeronScript@users.noreply.github.com>
2023-03-07 17:44:41 +0100

    Initial lab2 commit

---
+++ lab2/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+def my_printf(format_string, param):
+    shouldDo = True
+
+    for i in range(len(format_string)):
+        if shouldDo:
+            if format_string[i] == '#' and format_string[i+1] == 'k':
+                param = param.swapcase()
+                print(param,end="")
+                shouldDo=False
+            else:
+                print(format_string[i], end="")
+        else:
+            shouldDo=True
+    print("")
+
+data = sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
