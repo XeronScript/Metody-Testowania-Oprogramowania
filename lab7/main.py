44eb0bc3b0c53e3a280eb11ab980c01d3775abdb
Wartutel <82675884+XeronScript@users.noreply.github.com>
2023-04-25 17:37:55 +0200

    Find specifier, fix convert

--- lab7/main.py
+++ lab7/main.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def convert(c: chr):
-    return chr(ord(c) + 6)
+    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    if c in nums:
+        return c
+    else:
+        return chr(ord(c) + 6)
 
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+def my_printf(format_string, param):
+    pattern = r'#j'
+    search = re.search(pattern, format_string)
+    
+    if search is not None:
+        span = search.span()
+        arg = format_string[span[0]+1 : span[1]-1]
+    else:
+        print(format_string)
+
 
 data=sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
