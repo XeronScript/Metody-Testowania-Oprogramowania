5c6c3e01d0311327647e2bd01711e0e912a83447
Wartutel <82675884+XeronScript@users.noreply.github.com>
2023-05-16 17:50:19 +0200

    Change my_pritnf to use regex

--- lab8/main.py
+++ lab8/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 
 def convert_to_hex(param: str) -> str:
@@ -14,18 +15,8 @@ def convert_to_hex(param: str) -> str:
 
 
 def my_printf(format_string, param):
-    # print(format_string)
-    shouldDo = True
-    for idx in range(0, len(format_string)):
-        if shouldDo:
-            if format_string[idx] == "#" and format_string[idx + 1] == "k":
-                print(param, end="")
-                shouldDo = False
-            else:
-                print(format_string[idx], end="")
-        else:
-            shouldDo = True
-    print("")
+    pattern = r"#.Zj"
+    search = re.search(pattern, format_string)
 
 
 data = sys.stdin.readlines()
