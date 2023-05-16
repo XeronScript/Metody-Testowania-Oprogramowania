f781763bfc6e0e4e834a80c6709e9d90c2580c4d
Wartutel <82675884+XeronScript@users.noreply.github.com>
2023-05-16 18:16:43 +0200

    Handle Z in #.Zj format

--- lab8/main.py
+++ lab8/main.py
@@ -15,12 +15,20 @@ def convert_to_hex(param: str) -> str:
 
 
 def my_printf(format_string, param):
-    pattern = r"#.Zj"
+    pattern = r"#.(\d+)j"
     search = re.search(pattern, format_string)
 
     if search is not None:
+        l = int(search.group(1))
         new_param = convert_to_hex(str(hex(int(param)))[2:])
-        print(re.sub(pattern, new_param, format_string))
+
+        if l <= len(new_param):
+            print(re.sub(pattern, new_param, format_string))
+        else:
+            padded_param = "".join([0 for i in range(len(new_param - l))])
+            padded_param += new_param
+            print(re.sub(pattern, padded_param, format_string))
+
     else:
         print(format_string)
 
