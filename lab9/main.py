24a8d57b927cb444a471e471f6d6ab5fe473e3ac
Wartutel <82675884+XeronScript@users.noreply.github.com>
2023-05-23 17:52:19 +0200

    Full param convertion

--- lab9/main.py
+++ lab9/main.py
@@ -9,13 +9,13 @@ def split_float(num: str) -> tuple[int, int]:
     return (int(p), int(s))
 
 
-def convert_prefix(prefix: str) -> str | None:
+def convert_prefix(prefix: str) -> str:
     if not prefix.isdigit() or len(prefix) != 1:
-        return None
+        return ""
 
     prefix_int = int(prefix)
     if prefix_int < 0 or prefix_int > 9:
-        return None
+        return ""
 
     return chr(ord("a") + prefix_int)
 
@@ -29,11 +29,11 @@ def my_printf(format_string, param):
     search = re.search(pattern, format_string)
 
     if search is not None:
-        l = search.group(1)
-        prefix, suffix = split_float(l)
+        prefix, suffix = split_float(param)
         conv_prefix = convert_prefix(str(prefix))
-        conv_suffix = calculate_suffix(suffix)
-        new_param = conv_prefix
+        conv_suffix = str(calculate_suffix(suffix))
+        new_param = ".".join([conv_prefix, conv_suffix])
+        print(re.sub(pattern, new_param, format_string))
 
 
 data = sys.stdin.readlines()
