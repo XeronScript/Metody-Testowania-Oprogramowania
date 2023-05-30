9eb18b255462ad18d4609b3ebbaaafe67557957e
Wartutel <82675884+XeronScript@users.noreply.github.com>
2023-05-30 17:44:50 +0200

    Add another helper function and finish my_printf

--- lab10/main.py
+++ lab10/main.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
-def calculate_param_expression(param):
-    return int( (param*2) / len(str(param)) )
-    
+
+def getHexOrDecimal(param: int) -> str:
+    if param % 2 == 0:
+        return str(param)
+    else:
+        return hex(param).replace("0x", "")
+
+
+def calculate_param_expression(param: int):
+    return int((param * 2) / len(str(param)))
+
 
 def my_printf(format_string, param):
-    pass
-    
+    pattern = r"#a"
+    search = re.search(pattern, format_string)
 
-data=sys.stdin.readlines()
+    if search is not None:
+        new_param = calculate_param_expression(param)
+        hex_or_decimal = getHexOrDecimal(new_param)
+        print(re.sub(pattern, hex_or_decimal, format_string))
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i + 1].rstrip())
