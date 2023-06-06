3e9f819025ae9379793ab159c2f6ad1c12bea58e
XeronScript <ksasin3@gmail.com>
2023-06-06 17:48:25 +0200

    Create converting function functionality

--- lab11/main.py
+++ lab11/main.py
@@ -6,11 +6,21 @@ import re
 
 def convert_to_special_binary(param: str) -> str:
     binary = bin(int(param)).replace('-0b', '').replace('0b', '')
+    new_param = ''
+    possible_characters = 'abcdefghij'
+
+    for i, value in enumerate(binary[::-1]):
+        if value == '0':
+            new_param += '0'
+        else:
+            new_param += possible_characters[i % 10]
+    
+    return new_param.__reversed__()
 
 
 def my_printf(format_string,param):
     pattern = '#b'
-    
+
 
 data=sys.stdin.readlines()
 
